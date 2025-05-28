720488bb7f73d7b6c44eb9b1f7acffd0ac90a7e4
baejiwon <qowhxk@naver.com>
2025-05-28 11:42:24 +0900

    Article Repository Jpa / Query 전용 구분

---
+++ src/main/java/com/idol/board/repository/article/ArticleJpaRepository.java
@@ -0,0 +1,12 @@
+package com.idol.board.repository.article;
+
+import com.idol.board.domain.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface ArticleJpaRepository extends JpaRepository<Article, Long> {
+
+}
