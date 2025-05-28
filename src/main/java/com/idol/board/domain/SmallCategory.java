4b992a121544a0a6c7b1899fb645972284b90f3c
baejiwon <qowhxk@naver.com>
2025-05-28 11:43:14 +0900

    Enum 클래스 구현

---
+++ src/main/java/com/idol/board/domain/SmallCategory.java
@@ -0,0 +1,14 @@
+package com.idol.board.domain;
+
+public enum SmallCategory {
+                            // 같이 가요 하위 카테고리
+    BUSRENTAL_TYPE,         // 버스 대절
+    COMPANION_TYPE,         // 동행
+    AFTERPARTY_TYPE,        // 뒤풀이
+
+                            // 같이 해요 하위 카테고리
+    POPUP_TYPE,             // 팝업
+    BIRTHDAYCAFE_TYPE,      // 생일 카페
+    ONLINECONCERT_TYPE,     // 온라인 콘서트
+    FANDOMTOUR_TYPE         // 덕질 투어
+}
