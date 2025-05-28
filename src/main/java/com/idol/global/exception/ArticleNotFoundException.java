bc9463b03eda818c80d5a999695cf4b311b04338
baejiwon <qowhxk@naver.com>
2025-05-28 11:42:45 +0900

    Article Custom Error Exception 구현

---
+++ src/main/java/com/idol/global/exception/ArticleNotFoundException.java
@@ -0,0 +1,11 @@
+package com.idol.global.exception;
+
+public class ArticleNotFoundException extends RuntimeException {
+    public ArticleNotFoundException(String message) {
+        super(message);
+    }
+
+    public ArticleNotFoundException(Long articleId) {
+        super("게시물을 찾을 수 없습니다: " + articleId);
+    }
+} 
