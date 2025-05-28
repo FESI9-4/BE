978fa0e06f4b31a77d919bb19284d1e04a16aa5a
baejiwon <qowhxk@naver.com>
2025-05-28 11:45:42 +0900

    Auditing 설정 추가

--- src/main/java/com/idol/IdolApplication.java
+++ src/main/java/com/idol/IdolApplication.java
@@ -2,8 +2,10 @@ package com.idol;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class IdolApplication {
 
     public static void main(String[] args) {
