bc32d1a49e15af1dadf03c98822c1302b8e31ea1
baejiwon <qowhxk@naver.com>
2025-05-28 11:52:47 +0900

    S3 관련 이미지 업로드 및 호출 기능 구현

---
+++ src/main/java/com/idol/imageUpload/controller/S3Controller.java
@@ -0,0 +1,34 @@
+package com.idol.imageUpload.controller;
+
+import com.idol.global.common.response.ApiResponse;
+import com.idol.imageUpload.dto.GetS3UrlDto;
+import com.idol.imageUpload.useCase.ImageGetUserCase;
+import com.idol.imageUpload.useCase.ImageUploadUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/images")
+public class S3Controller {
+    private final ImageUploadUseCase imageUploadUseCase;
+    private final ImageGetUserCase imageGetUserCase;
+
+    // S3 이미지 파일 업로드할 URL 값 전송
+    @GetMapping( "/postImage")
+    // TODO :: User값 매개변수 추가 예정
+    public ApiResponse<GetS3UrlDto> getPostS3Url(String filename) {
+        Long userId = 1321414L;
+        GetS3UrlDto getS3UrlDto = imageUploadUseCase.getPostS3Url(userId, filename);
+        return ApiResponse.ok(getS3UrlDto, "이미지 주소 저장 성공");
+    }
+
+
+
+    @GetMapping( "/getImage")
+    public ApiResponse<GetS3UrlDto> getGetS3Url(@RequestParam String key) {
+        GetS3UrlDto getS3UrlDto = imageGetUserCase.getGetS3Url(key);
+
+        return ApiResponse.ok(getS3UrlDto, "이미지 주소 전송 성공");
+    }
+}
---
+++ src/main/java/com/idol/imageUpload/useCase/ImageUploadUseCase.java
@@ -0,0 +1,9 @@
+package com.idol.imageUpload.useCase;
+
+import com.idol.imageUpload.dto.GetS3UrlDto;
+
+import java.util.Date;
+
+public interface ImageUploadUseCase {
+    GetS3UrlDto getPostS3Url(Long memberId, String filename);
+}
