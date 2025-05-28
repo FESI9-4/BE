81f0626e0411869ad58757ac0d23078ad3bad19c
baejiwon <qowhxk@naver.com>
2025-05-28 11:52:56 +0900

    Location Entity 구현

---
+++ src/main/java/com/idol/board/domain/entity/Location.java
@@ -0,0 +1,38 @@
+package com.idol.board.domain.entity;
+
+import com.idol.board.dto.request.article.ArticleUpdateRequestDto;
+import com.idol.global.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Table(name = "location")
+@Getter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Where(clause = "is_deleted = false")
+public class Location extends BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "snowflake-id")
+    @GenericGenerator(name = "snowflake-id", strategy = "com.idol.global.common.snowflake.SnowflakeIdGenerator")
+    @Column(name = "location_id", nullable = false)
+    private Long locationId;
+
+    @Column(name = "latitude", nullable = false)
+    private double latitude;
+    @Column(name = "longitude", nullable = false)
+    private double longitude;
+    @Column(name = "road_name_address", nullable = false)
+    private String roadNameAddress;
+
+    public void update(ArticleUpdateRequestDto requestDto) {
+        if (requestDto.roadNameAddress() != null) this.roadNameAddress = requestDto.roadNameAddress();
+        if (requestDto.latitude() != null) this.latitude = requestDto.latitude();
+        if (requestDto.longitude() != null) this.longitude = requestDto.longitude();
+    }
+}
