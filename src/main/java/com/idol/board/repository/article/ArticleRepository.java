20b7993241af77e38a5992aa65c6449ad6f776a1
baejiwon <qowhxk@naver.com>
2025-05-28 11:42:24 +0900

    Article Repository Jpa / Query 전용 구분

---
+++ src/main/java/com/idol/board/repository/article/ArticleRepository.java
@@ -0,0 +1,9 @@
+package com.idol.board.repository.article;
+
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository {
+
+}
