9dbd9789b987988baf9895211cf629aa61a0edbe
baejiwon <qowhxk@naver.com>
2025-05-28 11:43:14 +0900

    Enum 클래스 구현

---
+++ src/main/java/com/idol/board/domain/BigCategory.java
@@ -0,0 +1,6 @@
+package com.idol.board.domain;
+
+public enum BigCategory {
+    GO_TYPE,            // 같이 가요
+    DOING_TYPE          // 같이 해요
+}
---
+++ src/main/java/com/idol/board/domain/UseStatus.java
@@ -0,0 +1,6 @@
+package com.idol.board.domain;
+
+public enum UseStatus {
+    UPCOMING_STATUS,        // 모임 예정
+    COMPLETED_STATUS,       // 모임 완료
+}
