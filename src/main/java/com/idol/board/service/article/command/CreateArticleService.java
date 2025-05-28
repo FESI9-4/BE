a00711871947898cf7db09ad1014bd7ff7f8dd64
baejiwon <qowhxk@naver.com>
2025-05-28 11:52:14 +0900

    Article Service CRUD 구현

---
+++ src/main/java/com/idol/board/service/article/command/CreateArticleService.java
@@ -0,0 +1,25 @@
+package com.idol.board.service.article.command;
+
+import com.idol.board.domain.entity.Article;
+import com.idol.board.dto.request.article.ArticleCreateRequestDto;
+import com.idol.board.repository.article.ArticleJpaRepository;
+import com.idol.board.usecase.article.command.CreateArticleUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class CreateArticleService implements CreateArticleUseCase {
+
+    private final ArticleJpaRepository articleJpaRepository;
+
+    @Override
+    @Transactional
+    public Long createArticle(ArticleCreateRequestDto requestDto, Long writerId) {
+        Article article = requestDto.toEntity(writerId);
+        articleJpaRepository.save(article);
+
+        return article.getArticleId();
+    }
+}
