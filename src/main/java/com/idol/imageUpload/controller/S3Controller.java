2739a3063a8210869a23829dc56c31968c07f5ff
baejiwon <qowhxk@naver.com>
2025-06-04 15:01:43 +0900

    String -> RequestDto 변경

--- src/main/java/com/idol/imageUpload/controller/S3Controller.java
+++ src/main/java/com/idol/imageUpload/controller/S3Controller.java
@@ -2,6 +2,7 @@ package com.idol.imageUpload.controller;
 
 import com.idol.global.common.response.ApiResponse;
 import com.idol.imageUpload.dto.GetS3UrlDto;
+import com.idol.imageUpload.dto.PostS3UrlDto;
 import com.idol.imageUpload.useCase.ImageGetUserCase;
 import com.idol.imageUpload.useCase.ImageUploadUseCase;
 import lombok.RequiredArgsConstructor;
@@ -15,11 +16,11 @@ public class S3Controller {
     private final ImageGetUserCase imageGetUserCase;
 
     // S3 이미지 파일 업로드할 URL 값 전송
-    @GetMapping( "/postImage")
+    @PostMapping( "/postImage")
     // TODO :: User값 매개변수 추가 예정
-    public ApiResponse<GetS3UrlDto> getPostS3Url(String filename) {
+    public ApiResponse<GetS3UrlDto> getPostS3Url(PostS3UrlDto request) {
         Long userId = 1321414L;
-        GetS3UrlDto getS3UrlDto = imageUploadUseCase.getPostS3Url(userId, filename);
+        GetS3UrlDto getS3UrlDto = imageUploadUseCase.getPostS3Url(userId, request.fileName());
         return ApiResponse.ok(getS3UrlDto, "이미지 주소 저장 성공");
     }
 
