5b2a98b195ce4100a4c146b2de4924a136aafb02
baejiwon <qowhxk@naver.com>
2025-06-04 14:57:22 +0900

    Comment Error Exception 생성

---
+++ src/main/java/com/idol/global/exception/CommentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.idol.global.exception;
+
+public class CommentNotFoundException extends RuntimeException {
+    public CommentNotFoundException(String message) {
+        super(message);
+    }
+
+    public CommentNotFoundException(Long commentId) {
+        super("해당 댓글을 찾을 수 없습니다: " + commentId);
+    }
+} 
