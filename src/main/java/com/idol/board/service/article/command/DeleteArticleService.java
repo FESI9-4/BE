d0cadc4e41c8bd648bc662fc9aa09e6e61e5906f
baejiwon <qowhxk@naver.com>
2025-06-04 14:59:49 +0900

    Repository 구조 변경에 따른 의존성 주입 이름 변경

--- src/main/java/com/idol/board/service/article/command/DeleteArticleService.java
+++ src/main/java/com/idol/board/service/article/command/DeleteArticleService.java
@@ -1,7 +1,8 @@
 package com.idol.board.service.article.command;
 
 import com.idol.board.domain.entity.Article;
-import com.idol.board.repository.article.ArticleJpaRepository;
+import com.idol.board.repository.article.ArticleRepository;
+import com.idol.board.repository.comment.CommentRepository;
 import com.idol.board.usecase.article.command.DeleteArticleUseCase;
 import com.idol.global.exception.ArticleNotFoundException;
 import jakarta.transaction.Transactional;
@@ -12,7 +13,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DeleteArticleService implements DeleteArticleUseCase {
 
-    private final ArticleJpaRepository articleJpaRepository;
+    private final ArticleRepository articleJpaRepository;
+    private final CommentRepository commentJpaRepository;
 
     @Override
     @Transactional
@@ -20,6 +22,10 @@ public class DeleteArticleService implements DeleteArticleUseCase {
         Article article = articleJpaRepository.findById(articleId)
                 .orElseThrow(() -> new ArticleNotFoundException(articleId));
 
+        // 1. 게시글과 연관된 댓글들을 모두 로드 후 삭제처리
+        commentJpaRepository.softDeleteAllByArticleId(articleId);
+
+        // 2. 게시글 삭제 처리
         article.markAsDeleted();
 
         return articleId;
