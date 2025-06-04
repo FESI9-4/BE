8337aef502bd0999e62aec58f88f4ca32b8e1b4f
baejiwon <qowhxk@naver.com>
2025-06-04 14:57:33 +0900

    구조 변경

--- src/main/java/com/idol/board/repository/comment/CommentJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.idol.board.repository.comment;
-
-import com.idol.board.domain.entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentJpaRepository extends JpaRepository<Comment, Long> {
-
-}
