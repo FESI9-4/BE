ba6046b4a5be35fa362a3ba5ee7fc87584c00646
baejiwon <qowhxk@naver.com>
2025-06-04 15:02:11 +0900

    Comment Update 기능 구현

---
+++ src/main/java/com/idol/board/usecase/comment/command/UpdateCommentUseCase.java
@@ -0,0 +1,8 @@
+package com.idol.board.usecase.comment.command;
+
+import com.idol.board.dto.request.comment.CommentCreateRequestDto;
+import com.idol.board.dto.request.comment.CommentUpdateRequestDto;
+
+public interface UpdateCommentUseCase {
+    Long updateComment(CommentUpdateRequestDto  commentUpdateRequestDto);
+}
