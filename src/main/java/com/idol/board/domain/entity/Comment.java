c96c5dfc7da0b415bfb76f96cfeeb0530dcd63d8
baejiwon <qowhxk@naver.com>
2025-06-04 14:56:10 +0900

    Comment Entity 생성 및 Article 맵핑

--- src/main/java/com/idol/board/domain/entity/Comment.java
+++ src/main/java/com/idol/board/domain/entity/Comment.java
@@ -1,19 +1,21 @@
 package com.idol.board.domain.entity;
 
 import com.idol.board.dto.request.comment.CommentCreateRequestDto;
+import com.idol.board.dto.request.comment.CommentUpdateRequestDto;
 import com.idol.global.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
 
 @Entity
 @Table(name = "comment")
 @Getter
+@ToString(exclude = "article")
 @Builder
-@ToString
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Where(clause = "is_deleted = false")
 public class Comment extends BaseEntity {
 
@@ -21,39 +23,32 @@ public class Comment extends BaseEntity {
     @Column(name = "comment_id", nullable = false)
     private Long commentId;
 
-    @Column(name = "content", nullable = false)
-    private String content;
-
-    @Column(name = "parent_comment_id", nullable = true)
-    private Long parentCommentId;
-
-    @Column(name = "parent_comment_delete_status", nullable = true)
-    private Long parentCommentDeleteStatus;
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "article_id")
+    @JoinColumn(name = "article_id", nullable = false)
     private Article article;
 
     @Column(name = "writer_id", nullable = false)
     private Long writerId;
 
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "parent_comment_id")
+    private Long parentCommentId;
+
     @Column(name = "secret", nullable = false)
     private boolean secret;
 
+//    public void setArticle(Article article) {
+//        this.article = article;
+//    }
 
-    // 현재 댓글이 최상위 댓글인지 확인
-    public boolean isRoot(){
+    public boolean isRoot() {
         return parentCommentId.longValue() == commentId;
     }
 
-
-    public Comment create(Article article, Long writerId, Long parentCommentId) {
-        return Comment.builder()
-                .content(this.content)
-                .parentCommentId(parentCommentId == null ? commentId : parentCommentId)
-                .article(article)
-                .writerId(writerId)
-                .secret(this.secret)
-                .build();
+    public void update(CommentUpdateRequestDto requestDto) {
+        if(requestDto.content() != null) this.content = requestDto.content();
+        this.secret = requestDto.secret();
     }
 }
