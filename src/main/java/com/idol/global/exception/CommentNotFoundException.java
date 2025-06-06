ba7532b05a71a9836ca58db5ee48fe8c7223b921
baejiwon <qowhxk@naver.com>
2025-06-06 22:54:45 +0900

    개행 추가

--- src/main/java/com/idol/global/exception/CommentNotFoundException.java
+++ src/main/java/com/idol/global/exception/CommentNotFoundException.java
@@ -8,4 +8,4 @@ public class CommentNotFoundException extends RuntimeException {
     public CommentNotFoundException(Long commentId) {
         super("해당 댓글을 찾을 수 없습니다: " + commentId);
     }
-} 
+}
