c8a9f5adb564be5b3a18fc3db17d0f2c7e477e3b
baejiwon <qowhxk@naver.com>
2025-06-06 22:58:20 +0900

    Location Repository 추가

---
+++ src/main/java/com/idol/board/repository/location/LocationRepository.java
@@ -0,0 +1,9 @@
+package com.idol.board.repository.location;
+
+import com.idol.board.domain.entity.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Long>{
+}
