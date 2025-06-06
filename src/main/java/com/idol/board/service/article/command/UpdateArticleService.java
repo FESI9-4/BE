26c91191d440156dd2b6e3bfa009d49904b65e16
baejiwon <qowhxk@naver.com>
2025-06-06 22:57:43 +0900

    연관관계 제거에 따른 데이터 저장 구조 변경

--- src/main/java/com/idol/board/service/article/command/UpdateArticleService.java
+++ src/main/java/com/idol/board/service/article/command/UpdateArticleService.java
@@ -4,6 +4,7 @@ import com.idol.board.domain.entity.Article;
 import com.idol.board.domain.entity.Location;
 import com.idol.board.dto.request.article.ArticleUpdateRequestDto;
 import com.idol.board.repository.article.ArticleRepository;
+import com.idol.board.repository.location.LocationRepository;
 import com.idol.board.usecase.article.command.UpdateArticleUseCase;
 import com.idol.global.exception.ArticleNotFoundException;
 import lombok.RequiredArgsConstructor;
@@ -14,21 +15,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class UpdateArticleService implements UpdateArticleUseCase {
 
-    private final ArticleRepository articleJpaRepository;
+    private final ArticleRepository articleRepository;
+    private final LocationRepository locationRepository;
 
     @Override
     @Transactional
     public Long updateArticle(ArticleUpdateRequestDto requestDto, Long writerId, Long articleId) {
-        Article article = articleJpaRepository.findById(articleId)
+        Article article = articleRepository.findById(articleId)
                 .orElseThrow(() -> new ArticleNotFoundException(articleId));
 
         // Article 필드 업데이트
         article.update(requestDto);
 
         // Location 업데이트
-        Location location = article.getLocation();
-        location.update(requestDto);
+        Location location = locationRepository.findById(article.getLocationId())
+                .orElseThrow(() -> new ArticleNotFoundException(article.getLocationId()));
 
+        location.update(requestDto);
         return article.getArticleId();
     }
 }
