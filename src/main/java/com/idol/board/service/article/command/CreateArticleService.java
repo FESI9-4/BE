bb912e69c24d5e675623594fe3b88d6653f4a7cd
baejiwon <qowhxk@naver.com>
2025-06-06 22:55:21 +0900

    연관관계 제거에 따른 데이터 저장 구조 변경

--- src/main/java/com/idol/board/service/article/command/CreateArticleService.java
+++ src/main/java/com/idol/board/service/article/command/CreateArticleService.java
@@ -1,8 +1,10 @@
 package com.idol.board.service.article.command;
 
 import com.idol.board.domain.entity.Article;
+import com.idol.board.domain.entity.Location;
 import com.idol.board.dto.request.article.ArticleCreateRequestDto;
 import com.idol.board.repository.article.ArticleRepository;
+import com.idol.board.repository.location.LocationRepository;
 import com.idol.board.usecase.article.command.CreateArticleUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,13 +14,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class CreateArticleService implements CreateArticleUseCase {
 
-    private final ArticleRepository articleJpaRepository;
+    private final ArticleRepository articleRepository;
+    private final LocationRepository locationRepository;
 
     @Override
     @Transactional
     public Long createArticle(ArticleCreateRequestDto requestDto, Long writerId) {
-        Article article = requestDto.toEntity(writerId);
-        articleJpaRepository.save(article);
+        Location location = locationRepository.save(
+                Location.builder()
+                        .latitude(requestDto.latitude())
+                        .longitude(requestDto.longitude())
+                        .roadNameAddress(requestDto.roadNameAddress())
+                        .build()
+        );
+
+
+        Article article = requestDto.toEntity(writerId,location.getLocationId());
+        articleRepository.save(article);
 
         return article.getArticleId();
     }
