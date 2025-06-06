3ff08683af4ee2c1569a7bed227d88476d1ad99a
baejiwon <qowhxk@naver.com>
2025-06-06 22:56:16 +0900

    JpaRepository -> Repository 이름 변경

--- src/main/java/com/idol/board/service/article/command/DeleteArticleService.java
+++ src/main/java/com/idol/board/service/article/command/DeleteArticleService.java
@@ -3,6 +3,7 @@ package com.idol.board.service.article.command;
 import com.idol.board.domain.entity.Article;
 import com.idol.board.repository.article.ArticleRepository;
 import com.idol.board.repository.comment.CommentRepository;
+import com.idol.board.repository.location.LocationRepository;
 import com.idol.board.usecase.article.command.DeleteArticleUseCase;
 import com.idol.global.exception.ArticleNotFoundException;
 import jakarta.transaction.Transactional;
@@ -13,19 +14,21 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class DeleteArticleService implements DeleteArticleUseCase {
 
-    private final ArticleRepository articleJpaRepository;
-    private final CommentRepository commentJpaRepository;
-
+    private final ArticleRepository articleRepository;
+    private final CommentRepository commentRepository;
+    private final LocationRepository locationRepository;
     @Override
     @Transactional
     public Long delete(Long articleId){
-        Article article = articleJpaRepository.findById(articleId)
+        Article article = articleRepository.findById(articleId)
                 .orElseThrow(() -> new ArticleNotFoundException(articleId));
 
-        // 1. 게시글과 연관된 댓글들을 모두 로드 후 삭제처리
-        commentJpaRepository.softDeleteAllByArticleId(articleId);
+        commentRepository.softDeleteAllByArticleId(articleId);
+        locationRepository.findById(article.getLocationId())
+                .ifPresent(location -> {
+                    location.markAsDeleted();
+                });
 
-        // 2. 게시글 삭제 처리
         article.markAsDeleted();
 
         return articleId;
