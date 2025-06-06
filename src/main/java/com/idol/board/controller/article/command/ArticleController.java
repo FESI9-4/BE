137bd45abc2cc1d2e8ff6a6528b1b09a4dad886b
baejiwon <qowhxk@naver.com>
2025-06-06 22:52:39 +0900

    Swagger 추가

--- src/main/java/com/idol/board/controller/article/command/ArticleController.java
+++ src/main/java/com/idol/board/controller/article/command/ArticleController.java
@@ -6,17 +6,21 @@ import com.idol.board.usecase.article.command.CreateArticleUseCase;
 import com.idol.board.usecase.article.command.DeleteArticleUseCase;
 import com.idol.board.usecase.article.command.UpdateArticleUseCase;
 import com.idol.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/board")
 @RequiredArgsConstructor
+@Tag(name = "게시글 CUD API", description = "게시글 CUD API")
 public class ArticleController {
     private final CreateArticleUseCase createArticleUseCase;
     private final DeleteArticleUseCase deleteArticleUseCase;
     private final UpdateArticleUseCase updateArticleUserCase;
 
+    @Operation(summary = "게시글 작성", description = "이미지, 주소, 게시글 내용 작성합니다.")
     @PostMapping()
     public ApiResponse<Long> createArticle(@RequestBody ArticleCreateRequestDto requestDto) {
         Long writerId = 1312441414L;
@@ -25,6 +29,7 @@ public class ArticleController {
     }
 
 
+    @Operation(summary = "게시글 삭제", description = "게시글 Soft Delete")
     @DeleteMapping("/{articleId}")
     public ApiResponse<Long> deleteArticle(@PathVariable("articleId") Long articleId) {
         Long resultId = deleteArticleUseCase.delete(articleId);
@@ -32,7 +37,7 @@ public class ArticleController {
         return ApiResponse.ok(resultId, "게시물 삭제가 완료되었습니다.");
     }
 
-
+    @Operation(summary = "게시글 수정", description = "게시글 수정")
     @PatchMapping("/{articleId}")
     public ApiResponse<Long> updateArticle(@RequestBody ArticleUpdateRequestDto requestDto, @PathVariable Long articleId) {
         Long writerId = 1312441414L; // TODO: 실제 인증된 사용자의 ID로 대체
--- src/main/java/com/idol/board/controller/comment/command/CommentController.java
+++ src/main/java/com/idol/board/controller/comment/command/CommentController.java
@@ -6,17 +6,21 @@ import com.idol.board.usecase.comment.command.CreateCommentUseCase;
 import com.idol.board.usecase.comment.command.DeleteCommentUseCase;
 import com.idol.board.usecase.comment.command.UpdateCommentUseCase;
 import com.idol.global.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/board")
 @RequiredArgsConstructor
+@Tag(name = "댓글 CUD API", description = "댓글 CUD API")
 public class CommentController {
     private final CreateCommentUseCase createCommentUseCase;
     private final DeleteCommentUseCase deleteCommentUseCase;
     private final UpdateCommentUseCase updateCommentUseCase;
 
+    @Operation(summary = "댓글 작성", description = "댓글 혹은 대댓글 작성합니다")
     @PostMapping("/{articleId}/comment")
     public ApiResponse<Long>  createComment(@PathVariable Long articleId, @RequestBody CommentCreateRequestDto requestDto) {
         // TODO :: 인증 연결
@@ -26,12 +30,14 @@ public class CommentController {
         return ApiResponse.ok(commentId, "댓글 생성 성공");
     }
 
+    @Operation(summary = "댓글 삭제", description = "댓글 Soft Delete")
     @DeleteMapping("/{articleId}/comment/{commentId}")
     public ApiResponse<Long> deleteComment(@PathVariable(name = "articleId") Long articleId, @PathVariable(name = "commentId") Long commentId) {
         deleteCommentUseCase.delete(commentId);
         return ApiResponse.ok(commentId, "댓글 삭제 성공");
     }
 
+    @Operation(summary = "댓글 수정", description = "댓글 수정")
     @PatchMapping("/{articleId}/comment")
     public ApiResponse<Long>  updateComment(@PathVariable Long articleId, @RequestBody CommentUpdateRequestDto requestDto) {
         // TODO :: 인증 연결
