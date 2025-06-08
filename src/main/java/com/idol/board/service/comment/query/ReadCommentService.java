beaf1ddbb468e77e06cf63081997180b3161e13a
baejiwon <qowhxk@naver.com>
2025-06-08 23:17:35 +0900

    @Transactional 통합

--- src/main/java/com/idol/board/service/comment/query/ReadCommentService.java
+++ src/main/java/com/idol/board/service/comment/query/ReadCommentService.java
@@ -1,23 +1,24 @@
 package com.idol.board.service.comment.query;
 
-import com.idol.board.domain.entity.Comment;
 import com.idol.board.dto.response.comment.CommentResponseDto;
 import com.idol.board.repository.comment.CommentRepository;
-import com.idol.board.repository.mapper.CommentReadDao;
+import com.idol.board.repository.mapper.CommentReadQueryResult;
 import com.idol.board.usecase.comment.query.ReadCommentUseCase;
-import com.idol.global.exception.CommentNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ReadCommentService implements ReadCommentUseCase {
     private final CommentRepository commentJpaRepository;
 
+    @Transactional(readOnly = true)
     public List<CommentResponseDto> readAll(Long articleId, Long lastParentCommentId, Long lastCommentId, Long limit){
-        List<CommentReadDao> comments = lastParentCommentId == null || lastCommentId == null ?
+        List<CommentReadQueryResult> comments = lastParentCommentId == null || lastCommentId == null ?
                 commentJpaRepository.findAllInfiniteScroll(articleId, limit) :
                 commentJpaRepository.findAllInfiniteScroll(articleId,lastParentCommentId,lastCommentId,limit);
 
