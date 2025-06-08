14ae9787e1561d4669d79fab34e577d33a447480
baejiwon <qowhxk@naver.com>
2025-06-08 23:16:52 +0900

    S3 의존성 변경에 따른 구조 변경

--- src/main/java/com/idol/imageUpload/service/command/ImageExpirationService.java
+++ src/main/java/com/idol/imageUpload/service/command/ImageExpirationService.java
@@ -3,16 +3,13 @@ package com.idol.imageUpload.service.command;
 import com.idol.imageUpload.useCase.ImageExpirationUseCase;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Date;
 
 @Service
 public class ImageExpirationService implements ImageExpirationUseCase {
     @Override
-    public Date getExpiration() {
-        Date expiration = new Date();
-        long expTimeMillis = expiration.getTime();
-        expTimeMillis += 1000 * 60 * 60; // 1시간으로 설정하기
-        expiration.setTime(expTimeMillis);
-        return expiration;
+    public Duration getExpirationDuration() {
+        return Duration.ofHours(1);
     }
 }
