1f425b5a1c4ca653128aee6bed7e9e380ac5adaf
baejiwon <qowhxk@naver.com>
2025-06-08 23:12:30 +0900

    Builder 제거 생성자 추가

--- src/main/java/com/idol/board/domain/entity/Location.java
+++ src/main/java/com/idol/board/domain/entity/Location.java
@@ -3,18 +3,15 @@ package com.idol.board.domain.entity;
 import com.idol.board.dto.request.article.ArticleUpdateRequestDto;
 import com.idol.global.common.entity.BaseEntity;
 import jakarta.persistence.*;
+import jakarta.persistence.Table;
 import lombok.*;
-import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.Where;
+import org.hibernate.annotations.*;
 
 @Entity
 @Table(name = "location")
 @Getter
 @ToString
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
-@Where(clause = "is_deleted = false")
 public class Location extends BaseEntity {
 
     @Id
@@ -29,6 +26,12 @@ public class Location extends BaseEntity {
     private double longitude;
     @Column(name = "road_name_address", nullable = false)
     private String roadNameAddress;
+
+    public Location(double latitude, double longitude, String roadNameAddress) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.roadNameAddress = roadNameAddress;
+    }
 
     public void update(ArticleUpdateRequestDto requestDto) {
         if (requestDto.roadNameAddress() != null) this.roadNameAddress = requestDto.roadNameAddress();
