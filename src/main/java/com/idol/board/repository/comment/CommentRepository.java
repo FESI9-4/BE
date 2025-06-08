4b84935179e72f5100dc6cefab3331d6615f276f
baejiwon <qowhxk@naver.com>
2025-06-08 23:14:02 +0900

    softDelete에 따른 조건 추가

--- src/main/java/com/idol/board/repository/comment/CommentRepository.java
+++ src/main/java/com/idol/board/repository/comment/CommentRepository.java
@@ -1,7 +1,16 @@
 package com.idol.board.repository.comment;
 
+import com.idol.board.domain.entity.Article;
 import com.idol.board.domain.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long>, CommentRepositoryCustom {
+    @Query("select a from Comment a where a.isDeleted = false and a.articleId = :articleId")
+    List<Comment> findByArticleId(Long articleId);
 }
--- src/main/java/com/idol/board/repository/comment/CommentRepositoryCustom.java
+++ src/main/java/com/idol/board/repository/comment/CommentRepositoryCustom.java
@@ -1,21 +1,21 @@
 package com.idol.board.repository.comment;
 
 import com.idol.board.domain.entity.Comment;
-import com.idol.board.repository.mapper.CommentReadDao;
-import org.springframework.stereotype.Repository;
-
+import com.idol.board.repository.mapper.CommentReadQueryResult;
 import java.util.List;
+import java.util.Optional;
 
-@Repository
+
 public interface CommentRepositoryCustom {
-    Long countBy(Long articleId, Long parentCommentId, Long limit);
 
-    // 부모 댓글 삭제 시 관련된 자식 댓글들도 완전 삭제
-    void deleteAllChildComments(Long parentCommentId);
+    Optional<Comment> findByCommentId(Long articleId);
+
+    Long relatedCommentcountBy(Long articleId, Long parentCommentId, Long limit);
 
     void softDeleteAllByArticleId(Long articleId);
 
-    List<CommentReadDao> findAllInfiniteScroll(Long articleId, Long limit);
-    List<CommentReadDao> findAllInfiniteScroll(Long articleId, Long lastParentCommentId, Long lastCommentId, Long limit);
+    List<CommentReadQueryResult> findAllInfiniteScroll(Long articleId, Long limit);
+    List<CommentReadQueryResult> findAllInfiniteScroll(Long articleId, Long lastParentCommentId, Long lastCommentId, Long limit);
 
+    Optional<Comment> findByParentCommentId(Long parentCommentId);
 }
