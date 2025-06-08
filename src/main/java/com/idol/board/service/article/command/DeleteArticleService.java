e4d3aab529f7d11da657575aa4c88c821af5d20c
baejiwon <qowhxk@naver.com>
2025-06-08 23:15:52 +0900

    @Transactional 통합 및 ErrorHandler 구조 변경

--- src/main/java/com/idol/board/service/article/command/DeleteArticleService.java
+++ src/main/java/com/idol/board/service/article/command/DeleteArticleService.java
@@ -1,29 +1,39 @@
 package com.idol.board.service.article.command;
 
 import com.idol.board.domain.entity.Article;
+import com.idol.board.domain.entity.Comment;
 import com.idol.board.repository.article.ArticleRepository;
 import com.idol.board.repository.comment.CommentRepository;
 import com.idol.board.repository.location.LocationRepository;
 import com.idol.board.usecase.article.command.DeleteArticleUseCase;
-import com.idol.global.exception.ArticleNotFoundException;
+import com.idol.global.exception.NotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+import static java.util.function.Predicate.not;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class DeleteArticleService implements DeleteArticleUseCase {
 
     private final ArticleRepository articleRepository;
     private final CommentRepository commentRepository;
     private final LocationRepository locationRepository;
-    @Override
-    @Transactional
-    public Long delete(Long articleId){
-        Article article = articleRepository.findById(articleId)
-                .orElseThrow(() -> new ArticleNotFoundException(articleId));
 
-        commentRepository.softDeleteAllByArticleId(articleId);
+    @Override
+    public Long delete(Long articleId){
+        Article article = articleRepository.findByArticleId(articleId)
+                .orElseThrow(() -> new NotFoundException("Article", articleId));
+
+
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+
+        comments.forEach(Comment::markAsDeleted);
+
         locationRepository.findById(article.getLocationId())
                 .ifPresent(location -> {
                     location.markAsDeleted();
--- src/main/java/com/idol/board/service/article/command/UpdateArticleService.java
+++ src/main/java/com/idol/board/service/article/command/UpdateArticleService.java
@@ -6,30 +6,33 @@ import com.idol.board.dto.request.article.ArticleUpdateRequestDto;
 import com.idol.board.repository.article.ArticleRepository;
 import com.idol.board.repository.location.LocationRepository;
 import com.idol.board.usecase.article.command.UpdateArticleUseCase;
-import com.idol.global.exception.ArticleNotFoundException;
+import com.idol.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import static java.util.function.Predicate.not;
+
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class UpdateArticleService implements UpdateArticleUseCase {
 
     private final ArticleRepository articleRepository;
     private final LocationRepository locationRepository;
 
     @Override
-    @Transactional
     public Long updateArticle(ArticleUpdateRequestDto requestDto, Long writerId, Long articleId) {
-        Article article = articleRepository.findById(articleId)
-                .orElseThrow(() -> new ArticleNotFoundException(articleId));
+        Article article = articleRepository.findByArticleId(articleId)
+                .filter(not(Article::getIsDeleted))
+                .orElseThrow(() -> new NotFoundException("Article", articleId));
 
         // Article 필드 업데이트
         article.update(requestDto);
 
         // Location 업데이트
-        Location location = locationRepository.findById(article.getLocationId())
-                .orElseThrow(() -> new ArticleNotFoundException(article.getLocationId()));
+        Location location = locationRepository.findByLocationId(article.getLocationId())
+                .orElseThrow(() -> new NotFoundException("Article", articleId));
 
         location.update(requestDto);
         return article.getArticleId();
