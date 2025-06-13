e32e7b793556f921e8cb8de44603da6d8aad8354
baejiwon <qowhxk@naver.com>
2025-06-13 23:54:06 +0900

    QueryDSL 변경

--- src/main/java/com/idol/board/repository/comment/CommentRepositoryImpl.java
+++ src/main/java/com/idol/board/repository/comment/CommentRepositoryImpl.java
@@ -23,56 +23,88 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
 
     private final EntityManager entityManager;
     private final JPAQueryFactory queryFactory;
+    private static final QComment comment = QComment.comment;
 
     @Override
     public Optional<Comment> findByCommentId(Long commentId) {
-        String sql = "select a from Comment a where a.isDeleted = false and a.commentId = :commentId";
-        Query query = entityManager.createQuery(sql)
-                .setParameter("commentId", commentId);
-        try {
-            return Optional.ofNullable((Comment) query.getSingleResult());
-        } catch (NoResultException e) {
-            return Optional.empty();
-        }
+
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(comment)
+                        .where(
+                                comment.isDeleted.eq(false),
+                                comment.commentId.eq(commentId)
+                        )
+                        .fetchOne()
+        );
     }
+
+
+//    @Override
+//    public Optional<Comment> findByParentCommentId(Long parentCommentId) {
+//        String sql = "select a from Comment a where a.isDeleted = false and a.parentCommentId = :parentCommentId";
+//        Query query = entityManager.createQuery(sql)
+//                .setParameter("parentCommentId", parentCommentId);
+//        try {
+//            return Optional.ofNullable((Comment) query.getSingleResult());
+//        } catch (NoResultException e) {
+//            return Optional.empty();
+//        }
+//    }
+
 
     @Override
     public Optional<Comment> findByParentCommentId(Long parentCommentId) {
-        String sql = "select a from Comment a where a.isDeleted = false and a.parentCommentId = :parentCommentId";
-        Query query = entityManager.createQuery(sql)
-                .setParameter("parentCommentId", parentCommentId);
-        try {
-            return Optional.ofNullable((Comment) query.getSingleResult());
-        } catch (NoResultException e) {
-            return Optional.empty();
-        }
+
+        return Optional.ofNullable(
+                queryFactory
+                .selectFrom(comment)
+                .where(
+                        comment.isDeleted.eq(false),
+                        comment.parentCommentId.eq(parentCommentId)
+                )
+                .fetchOne()
+        );
     }
 
-
     // 연관된 댓글 개수 확인
+//    @Override
+//    public Long relatedCommentCountBy(Long articleId, Long parentCommentId, Long limit) {
+//        String sql = "select count(*) from(" +
+//                "   select comment_id from comment" +
+//                "   where is_deleted = false and article_id = :articleId and parent_comment_id = :parentCommentId" +
+//                "   limit :limit" +
+//                ") t";
+//
+//        Query query = entityManager.createNativeQuery(sql)
+//                .setParameter("articleId", articleId)
+//                .setParameter("parentCommentId", parentCommentId)
+//                .setParameter("limit", limit);
+//
+//
+//        log.info(String.valueOf(((Number) query.getSingleResult()).longValue()));
+//        return ((Number) query.getSingleResult()).longValue();
+//    }
+
     @Override
-    public Long relatedCommentcountBy(Long articleId, Long parentCommentId, Long limit) {
-        String sql = "select count(*) from(" +
-                "   select comment_id from comment" +
-                "   where is_deleted = false and article_id = :articleId and parent_comment_id = :parentCommentId" +
-                "   limit :limit" +
-                ") t";
-
-        Query query = entityManager.createNativeQuery(sql)
-                .setParameter("articleId", articleId)
-                .setParameter("parentCommentId", parentCommentId)
-                .setParameter("limit", limit);
-
-
-        log.info(String.valueOf(((Number) query.getSingleResult()).longValue()));
-        return ((Number) query.getSingleResult()).longValue();
+    public Long relatedCommentCountBy(Long articleId, Long parentCommentId, Long limit) {
+        return queryFactory
+                .select(comment.count())
+                .from(comment)
+                .where(
+                        comment.isDeleted.eq(false),
+                        comment.articleId.eq(articleId),
+                        comment.parentCommentId.eq(parentCommentId)
+                )
+                .limit(limit)
+                .fetchOne();
     }
 
     // 맨 처음 데이터 출력
     @Override
     public List<CommentReadQueryResult> findAllInfiniteScroll(Long articleId, Long limit) {
-        QComment comment = QComment.comment;
 
+//        QComment comment = QComment.comment;
         return queryFactory
                 .select(Projections.constructor(CommentReadQueryResult.class,
                         comment.commentId,
@@ -103,7 +135,7 @@ public class CommentRepositoryImpl implements CommentRepositoryCustom {
     // 해당 코드에서 parent_comment_id값이 출력되는 댓글의 마지막 값인 lastParentCommentId보다 같으면서 commentId값이 lastCommentId보다 큰 경우를 찾아 대댓글까지 출력시킴
     @Override
     public List<CommentReadQueryResult> findAllInfiniteScroll(Long articleId, Long lastParentCommentId, Long lastCommentId, Long limit) {
-        QComment comment = QComment.comment;
+//        QComment comment = QComment.comment;
 
         return queryFactory
                 .select(Projections.constructor(CommentReadQueryResult.class,
