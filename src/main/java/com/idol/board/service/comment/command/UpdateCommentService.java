90d5c04fc6300407a7d7ee27e593df3045e79dc4
baejiwon <qowhxk@naver.com>
2025-06-14 09:27:21 +0900

    유저 인증 검사 추가

--- src/main/java/com/idol/board/service/comment/command/UpdateCommentService.java
+++ src/main/java/com/idol/board/service/comment/command/UpdateCommentService.java
@@ -1,9 +1,11 @@
 package com.idol.board.service.comment.command;
 
+import com.idol.board.domain.entity.Article;
 import com.idol.board.domain.entity.Comment;
 import com.idol.board.dto.request.comment.CommentUpdateRequestDto;
 import com.idol.board.repository.comment.CommentRepository;
 import com.idol.board.usecase.comment.command.UpdateCommentUseCase;
+import com.idol.global.exception.IllegalArgumentException;
 import com.idol.global.exception.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,13 +19,21 @@ public class UpdateCommentService implements UpdateCommentUseCase {
     private final CommentRepository commentRepository;
 
     @Override
-    public Long updateComment(CommentUpdateRequestDto commentUpdateRequestDto) {
+    public Long updateComment(CommentUpdateRequestDto commentUpdateRequestDto, Long writerId) {
         Comment comment = commentRepository.findByCommentId(commentUpdateRequestDto.commentId())
                 .orElseThrow(() -> new NotFoundException("Comment", commentUpdateRequestDto.commentId()));
+
+        validateUserHasPermission(comment,writerId);
 
         comment.update(commentUpdateRequestDto);
 
         return comment.getCommentId();
+    }
+
+    private void validateUserHasPermission(Comment comment, Long writerId) {
+        if (!comment.getWriterId().equals(writerId)) {
+            throw new IllegalArgumentException("Comment",comment.getCommentId());
+        }
     }
 
 }
