e3002da81c51f68ae57d9278a6acb6a8ffcb903f
seojin Yoon <90759319+7zrv@users.noreply.github.com>
2025-06-15 17:05:30 +0900

    Feat/13 add permitall url (#17)
    * feat: snowflake 적용 리팩토링
    
    * feat: snowflake 적용 리팩토링
    
    * feat: add origin
    
    * feat: add origin

--- src/main/java/com/idol/global/config/SecurityConfig.java
+++ src/main/java/com/idol/global/config/SecurityConfig.java
@@ -68,7 +68,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        //configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.addAllowedOrigin("*");
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
