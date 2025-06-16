ad60ff25c29212cbc3373b6f97d680ed294a5828
baejiwon <qowhxk@naver.com>
2025-06-16 22:13:42 +0900

    게시글 전체 조회 기능 구현

--- src/main/java/com/idol/board/service/article/query/ReadArticleService.java
+++ src/main/java/com/idol/board/service/article/query/ReadArticleService.java
@@ -1,32 +1,41 @@
 package com.idol.board.service.article.query;
 
+import com.idol.board.domain.BigCategory;
 import com.idol.board.domain.OpenStatus;
+import com.idol.board.domain.SmallCategory;
 import com.idol.board.domain.entity.Article;
 import com.idol.board.domain.entity.Location;
 import com.idol.board.domain.entity.Participant;
+import com.idol.board.dto.response.article.ArticleListResponseDto;
 import com.idol.board.dto.response.article.ArticleReadResponseDto;
 import com.idol.board.dto.response.participant.ParticipantResponseDto;
 import com.idol.board.repository.article.ArticleRepository;
 import com.idol.board.repository.location.LocationRepository;
+import com.idol.board.repository.mapper.ArticleListReadQueryResult;
 import com.idol.board.usecase.article.query.ReadArticleUseCase;
 import com.idol.global.exception.NotFoundException;
 import com.idol.imageUpload.dto.GetS3UrlDto;
 import com.idol.imageUpload.useCase.ImageGetUserCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional()
 public class ReadArticleService implements ReadArticleUseCase {
 
     private final ArticleRepository articleRepository;
-    private final LocationRepository locationRepsitory;
+    private final LocationRepository locationRepository;
     private final ImageGetUserCase imageGetUserCase;
 
 
     @Override
+    @Transactional(readOnly = true)
     public ArticleReadResponseDto readArticle(Long articleId) {
         Article article = articleRepository.findByArticleId(articleId)
                 .orElseThrow(() -> new NotFoundException("Article", articleId));
@@ -37,9 +46,9 @@ public class ReadArticleService implements ReadArticleUseCase {
            2. 현재 게시물 찜되어 있는지 확인
          */
 
-        GetS3UrlDto getS3UrlDto = imageGetUserCase.getGetS3Url(article.getArticleImageKey());
-        Location location = locationRepsitory.findByLocationId(article.getLocationId())
-                .orElseThrow(() -> new NotFoundException("Location", article.getLocationId()));
+        GetS3UrlDto getS3UrlDto = getS3Url(article.getArticleImageKey());
+
+        Location location = validateLocation(article.getLocationId());
 
         List<ParticipantResponseDto> participants = null;
 
@@ -50,9 +59,38 @@ public class ReadArticleService implements ReadArticleUseCase {
         return dto;
     }
 
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ArticleListResponseDto> searchArticleList(
+            BigCategory bigCategory, SmallCategory smallCategory, String location,
+            Timestamp date, String sort, boolean sortAsc, Long limit, Long page) {
+
+        List<ArticleListResponseDto> searchArticles = articleRepository.findArticleList(
+                bigCategory, smallCategory, location, date, sort,  sortAsc, limit,  (page -1) * limit).stream()
+                .map(result ->
+                        ArticleListResponseDto.from(
+                                result,
+                                validateLocation(result.locationId()).getRoadNameAddress(),
+                                getS3Url(result.imageKey()).preSignedUrl()
+                        ))
+                .collect(Collectors.toList());
+
+        return searchArticles;
+    }
+
     private void validateCheckOpenStatus(Article article) {
         if(article.getCurrentPerson() >= article.getMinPerson()){
             article.updateOpenStatus(OpenStatus.CONFIRMED_STATUS);
         }
+    }
+
+    private Location validateLocation(Long locationId) {
+        return locationRepository.findByLocationId(locationId)
+                .orElseThrow(() -> new NotFoundException("Location", locationId));
+    }
+
+    private GetS3UrlDto getS3Url(String imageKey){
+        return imageGetUserCase.getGetS3Url(imageKey);
     }
 }
--- src/main/java/com/idol/board/usecase/article/query/ReadArticleUseCase.java
+++ src/main/java/com/idol/board/usecase/article/query/ReadArticleUseCase.java
@@ -1,9 +1,18 @@
 package com.idol.board.usecase.article.query;
 
+import com.idol.board.domain.BigCategory;
+import com.idol.board.domain.SmallCategory;
 import com.idol.board.domain.entity.Article;
+import com.idol.board.dto.response.article.ArticleListResponseDto;
 import com.idol.board.dto.response.article.ArticleReadResponseDto;
 import com.idol.board.dto.response.participant.ParticipantResponseDto;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.Timestamp;
+import java.util.List;
 
 public interface ReadArticleUseCase {
     ArticleReadResponseDto readArticle(Long articleId);
+
+    List<ArticleListResponseDto> searchArticleList(BigCategory bigCategory, SmallCategory smallCategory, String location, Timestamp date, String sort, boolean sortAsc, Long limit, Long page);
 }
