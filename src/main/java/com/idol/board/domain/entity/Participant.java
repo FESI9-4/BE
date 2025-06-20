36b6e7348883972f63cebf5cf0ee25f3d1962f9d
baejiwon <qowhxk@naver.com>
2025-06-20 10:02:28 +0900

    Hibernate 버전에 따른 ID값 생성 구조 변경

--- src/main/java/com/idol/board/domain/entity/Participant.java
+++ src/main/java/com/idol/board/domain/entity/Participant.java
@@ -1,18 +1,9 @@
 package com.idol.board.domain.entity;
 
-<<<<<<< HEAD
-import com.idol.board.domain.UseStatus;
+
 import com.idol.global.common.entity.BaseEntity;
-import com.idol.global.common.snowflake.Snowflake;
 import jakarta.persistence.*;
 import lombok.*;
-=======
-import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
->>>>>>> d88981d (참여자 관련 코드 구현)
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
@@ -21,15 +12,8 @@ import org.hibernate.annotations.GenericGenerator;
 @ToString
 // 생성자마다 builder 사용
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-<<<<<<< HEAD
 public class Participant extends BaseEntity {
     @Id
-=======
-public class Participant {
-    @Id
-    @GeneratedValue(generator = "snowflake-id")
-    @GenericGenerator(name = "snowflake-id", strategy = "com.idol.global.common.snowflake.SnowflakeIdGenerator")
->>>>>>> d88981d (참여자 관련 코드 구현)
     @Column(name = "participant_id", nullable = false)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long participantId;
@@ -42,7 +26,6 @@ public class Participant {
 
     @Column(name = "nickname", nullable = false)
     private String nickname;
-<<<<<<< HEAD
 
     @Column(name = "writer_id", nullable = false)
     private Long writerId;
@@ -54,6 +37,5 @@ public class Participant {
         this.nickname = participantNickname;
         this.writerId = writerId;
     }
-=======
->>>>>>> d88981d (참여자 관련 코드 구현)
+
 }
