52090da138eb41418f3038353dac2dfeb57b3367
JIWON <82360230+qowl880@users.noreply.github.com>
2025-06-24 00:28:59 +0900

    Feat/add mypage (#48)
    * Article Repository Jpa / Query 전용 구분
    
    * Article Custom Error Exception 구현
    
    * Auditing 설정 추가
    
    * Article Create Controller 구현
    
    * Article Delete Controller 구현
    
    * Article Controller CUD 구현
    
    * Comment Repository Jpa / Query 전용 구분
    
    * Comment Controller Create 구현
    
    * application yml파일 배포/로컬 분리
    
    # Conflicts:
    #	src/main/resources/application.yml
    
    * Article Create Controller 구현
    
    * Article Delete Controller 구현
    
    * Article Controller CUD 구현
    
    * application.yml 통합
    
    * Article Repository 설정
    
    * Comment Error Exception 생성
    
    * 구조 변경
    
    * Comment Read 전용 Dao 생성
    
    * application yml파일 배포/로컬 분리
    
    * Article Repository Jpa / Query 전용 구분
    
    * Article Create Controller 구현
    
    * Article Delete Controller 구현
    
    * Article Controller CUD 구현
    
    * application.yml 통합
    
    * Article Repository 설정
    
    * 개행 추가
    
    * ErrorException 통합
    
    * Dao -> QueryResult 파일명 변경
    
    * Update IdolApplication.java
    
    * 유저 인증 검사 추가
    
    * Article Entity 구현
    
    * Article Repository Jpa / Query 전용 구분
    
    * Article Custom Error Exception 구현
    
    * Auditing 설정 추가
    
    * Article Create Controller 구현
    
    * Article Delete Controller 구현
    
    * Article Controller CUD 구현
    
    * S3 관련 이미지 업로드 및 호출 기능 구현
    
    * Comment Repository Jpa / Query 전용 구분
    
    * Comment Controller Create 구현
    
    * application yml파일 배포/로컬 분리
    
    # Conflicts:
    #	src/main/resources/application.yml
    
    * Article Create Controller 구현
    
    * Article Delete Controller 구현
    
    * Article Controller CUD 구현
    
    * application.yml 통합
    
    * Article Repository 설정
    
    * Comment Error Exception 생성
    
    * 구조 변경
    
    * Comment Read 전용 Dao 생성
    
    * application yml파일 배포/로컬 분리
    
    * Article Repository Jpa / Query 전용 구분
    
    * Article Create Controller 구현
    
    * Article Delete Controller 구현
    
    * Article Controller CUD 구현
    
    * application.yml 통합
    
    * SoftDelete 추가
    
    * Article Repository 설정
    
    * 개행 추가
    
    * ErrorException 통합
    
    * Dao -> QueryResult 파일명 변경
    
    * S3 의존성 변경에 따른 구조 변경
    
    * Update IdolApplication.java
    
    * 유저 인증 검사 추가
    
    * @Where 삭제
    
    * 오류로 인한 주석처리
    
    * aws 관련 설정 추가
    
    * Transactional Read, Write 분리

---
+++ src/main/java/com/idol/board/service/article/query/ArticleStatusService.java
@@ -0,0 +1,49 @@
+package com.idol.board.service.article.query;
+
+import com.idol.board.domain.OpenStatus;
+import com.idol.board.domain.entity.Article;
+import com.idol.board.repository.article.ArticleRepository;
+import com.idol.global.exception.NotFoundException;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleStatusService {
+    private final ArticleRepository articleRepository;
+    private final EntityManager entityManager;
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public void validateCheckOpenStatus(Article article) {
+        if(article.getCurrentPerson() >= article.getMinPerson()){
+            if(article.getOpenStatus().equals(OpenStatus.PENDING_STATUS)) {
+                article.updateOpenStatus(OpenStatus.CONFIRMED_STATUS);
+            }
+        }
+    }
+
+
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    public OpenStatus validateCheckDeadlineStatus(Long articleId, OpenStatus status, Date deadline) {
+        Article article = articleRepository.findByArticleId(articleId)
+                .orElseThrow(() -> new NotFoundException("Article", articleId));
+
+        Timestamp deadlineTime = new Timestamp(deadline.getTime());
+        if(deadlineTime.before(new Timestamp(System.currentTimeMillis()))){
+            if(status.equals(OpenStatus.CONFIRMED_STATUS)) {
+                article.updateOpenStatus(OpenStatus.DEADLINE_STATUS);
+                return OpenStatus.DEADLINE_STATUS;
+            }else if(status.equals(OpenStatus.PENDING_STATUS)){
+                article.updateOpenStatus(OpenStatus.CANCELED_STATUS);
+                return OpenStatus.CANCELED_STATUS;
+            }
+        }
+        return status;
+    }
+}
